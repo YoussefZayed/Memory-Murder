709c88229e89e9cd829694951057635ce089484f
deestroy <dhritiaravind@gmail.com>
2020-01-12 02:07:43 -0500

    created table

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 # its the script to rule them all 
 
 from databaseBuilder import DatabaseBuilder
+from parjason import *
+
+test = DatabaseBuilder()
+#print(test.dataReturnIf(["NAME"],[["integer"]], 0, "France"))
+f = set_location('Cuhacking-2020/dat.json')
+test.deleteTable('h')
+test.createTable('h', types())
+test.insertTable('h', f)
+#test.createTable('Json_Data', f)
