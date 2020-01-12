9827443af077f7021aa73db6940f7b4384cac64a
deestroy <dhritiaravind@gmail.com>
2020-01-12 02:31:32 -0500

    created table updated

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from parjason import *
 
 test = DatabaseBuilder()
 #print(test.dataReturnIf(["NAME"],[["integer"]], 0, "France"))
-f = set_location('Cuhacking-2020/dat.json')
-test.deleteTable('h')
-test.createTable('h', types())
-test.insertTable('h', f)
+f = set_location('Cuhacking-2020/mdat.json')
+#test.deleteTable('h')
+test.createTable('testing', types())
+test.insertTable('testing', f)
 #test.createTable('Json_Data', f)
